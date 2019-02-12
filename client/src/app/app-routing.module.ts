20525059ae62eb69b43159fff622e5b96f344650
Akshaya Deo <akshaya.deo@webdunia.net>
2019-02-12 15:55:10 +0530

    chagned with Ticket form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserDetailComponent } from './component/user/user-detail/user-detail.component';
 import { AddUserComponent } from './component/user/add-user/add-user.component';
 import { UpdateUserComponent } from './component/user/update-user/update-user.component';
+import { TicketCreationFormComponent } from './component/shared/ticket-creation-form/ticket-creation-form.component';
+
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: UserDetailComponent },
   { path: 'create', component: AddUserComponent },
-  { path: 'update/:id', component: UpdateUserComponent }
+  { path: 'update/:id', component: UpdateUserComponent },
+  { path: 'ticket', component: TicketCreationFormComponent }
+
 ];
 
 
--- client/src/app/component/shared/ticket-creation-form/ticket-creation-form.component.ts
+++ client/src/app/component/shared/ticket-creation-form/ticket-creation-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+import { formControlBinding } from '@angular/forms/src/directives/reactive_directives/form_control_directive';
+
 
 @Component({
   selector: 'app-ticket-creation-form',
@@ -6,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-creation-form.component.css']
 })
 export class TicketCreationFormComponent implements OnInit {
+
+  ticketForm = new FormGroup({
+     serviceDesk:new FormControl(),
+     priority:new FormControl(),
+    issueTitle :new FormControl(),
+   description : new FormControl(), 
+  });
+  serviceDeskOptions = ["Desk 1","Desk 2","Desk 3","Desk 4"];
+  priorityOptions = ["1","2","3","4"];
 
   constructor() { }
 
