a3cff230c015ce2cf5821c4f39c9ffe97a9100d1
Akshaya Deo <akshaya.deo@webdunia.net>
2019-02-13 12:12:07 +0530

    Role Local  add/Edit added
    Role Local  add/Edit added

--- client/src/app/component/HR-CEO/role-create-update/role-create-update.component.ts
+++ client/src/app/component/HR-CEO/role-create-update/role-create-update.component.ts
@@ -11,9 +11,15 @@ import { formControlBinding } from '@angular/forms/src/directives/reactive_direc
 
 export class RoleCreateUpdateComponent implements OnInit {
 
+  names =["123",'789',"887777",'99999'];
+  roles = []
+  editIndex = null;
+
   roleForm = new FormGroup(
     {
       name:new FormControl(),
+      createdBy:new FormControl(),
+      description :new FormControl(),
       isActive:new FormControl(),
     }
   )
@@ -21,6 +27,37 @@ export class RoleCreateUpdateComponent implements OnInit {
   addUser()
   {
 
+    if(this.editIndex==null)
+    {
+      this.roles.push (this.roleForm.value);
+    }
+    else
+    {
+      this.roles.pop[this.editIndex]
+
+      this.roles[this.editIndex]=this.roleForm.value;
+    }
+    
+    this.resetRoleForm();
+    
+  }
+
+  resetRoleForm ()
+  {
+    this.roleForm.reset();
+    this.editIndex = null;
+  }
+
+  editRole(index)
+  {
+    this.roleForm.setValue(this.roles[index]);
+    this.editIndex = index;
+  }
+
+  deactivateRole(index)
+  {
+    let user = this.roles[index];
+    user.isActive = !user.isActive;
   }
 
   constructor() { }
