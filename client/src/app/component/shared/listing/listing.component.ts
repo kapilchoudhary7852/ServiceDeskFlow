49562ac1aca4e049404420a70c3b9056ce63abe4
Akshaya Deo <akshaya.deo@webdunia.net>
2019-02-18 18:37:11 +0530

    Search added
    Search done on the basis of deskId and status.

--- client/src/app/component/shared/listing/listing.component.ts
+++ client/src/app/component/shared/listing/listing.component.ts
@@ -5,6 +5,7 @@ import { TicketService } from 'src/app/service/ticket.service';
 import { Ticket } from '../../../model/ticket';
 import { Router } from '@angular/router';
 import { JsonPipe } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listing',
@@ -13,7 +14,17 @@ import { JsonPipe } from '@angular/common';
 })
 export class ListingComponent implements OnInit {
 
+  serviceDeskInput = '';
+  ticketInput = '';
+  employeeInput = '';
+  statusInput=''
+  ticketDateInput=''
+  resolvedDateInput=''
+  assignerInput=''
+
   tickets = []
+  filteredTickets = []
+  unFilteredTickets = []
   constructor(private formBuilder:FormBuilder, private ticketService: TicketService, private router: Router) { 
 
   }
@@ -23,10 +34,42 @@ export class ListingComponent implements OnInit {
     this.getAllTickets();
   }
 
+
+  filterTickets ()
+  {
+    var arryFiltered = this.unFilteredTickets;
+
+    if (this.serviceDeskInput != '') 
+    {
+      arryFiltered =  arryFiltered.filter(item=> item.ServiceDeskId==this.serviceDeskInput)
+    }
+    
+    if (this.statusInput != '') 
+    {
+      arryFiltered =  arryFiltered.filter(item=> item.Status==this.statusInput)
+    }
+     
+    this.tickets = arryFiltered;
+  }
+
+  resetFilters()
+  {
+    this.serviceDeskInput = '';
+    this.ticketInput = '';
+    this.employeeInput = '';
+    this.statusInput='';
+    this.ticketDateInput='';
+    this.resolvedDateInput='';
+    this.assignerInput='';
+    
+    this.getAllTickets();
+  }
+
   getAllTickets()
   {
     this.ticketService.getTickets().subscribe(data => {
       this.tickets = data;
+      this.unFilteredTickets = data;
       console.log(data);
     });
   }
